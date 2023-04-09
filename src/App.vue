958bb52a8f7a835f0b7ddffbd979205124de5c56
Blessing Onwunali <cheedinmarblessing@gmail.com>
2023-04-09 13:57:30 +0100

    feat:  card has front and back can be flipped

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
   <h1>Peek a vue</h1>
   <section class="game-board">
-   <base-card v-for="(card, index) in CardList" :value="card" :key="`card-${index}`"/>
+    <base-card
+      v-for="(card, index) in CardList"
+      :value="card.value"
+      :visible="card.visible"
+      :key="`card-${index}`"
+      :position="card.position"
+      @select-card="flipCard"
+    />
   </section>
 </template>
 
 <script>
-import BaseCard from './components/BaseCard.vue';
+import BaseCard from "./components/BaseCard.vue";
+import {ref} from "vue"
 export default {
-  components:{
-    BaseCard
+  components: {
+    BaseCard,
   },
-  setup(){
-    const CardList =[];
-    for (let i=0; i<16; i++){
-      CardList.push(i)
+  setup() {
+    const CardList = ref([]);
+    for (let i = 0; i < 16; i++) {
+      CardList.value.push({
+        value: i,
+        visible: false,
+        position:i
+      });
     }
-    return{
-      CardList
+    const flipCard = (payload)=>{
+      CardList.value[payload.position].visible= true
     }
-  }
-}
+    return {
+      CardList,flipCard
+    };
+  },
+};
 </script>
 
 <style>
@@ -32,14 +47,14 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-.card{
-  border: 5px solid #ccc
+.card {
+  border: 5px solid #ccc;
 }
-.game-board{
+.game-board {
   display: grid;
   grid-template-columns: 100px 100px 100px 100px;
-  grid-column-gap:30px ;
-  grid-template-rows:100px 100px 100px 100px;
+  grid-column-gap: 30px;
+  grid-template-rows: 100px 100px 100px 100px;
   grid-row-gap: 30px;
   justify-content: center;
 }
--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -1,6 +1,8 @@
 <template>
- <div class="card">
-  {{ value }}
+ <div class="card" @click="selectCard">
+  <div class="card-face is-front" v-if="visible">{{ value }}</div>
+  <div class="card-face is-back" v-else>Back</div>
+  
  </div>
 </template>
 
@@ -10,12 +12,48 @@ export default {
     value:{
       type:Number,
       required:true
+    },
+    visible:{
+      type: Boolean,
+      default:false
+    },
+    position:{
+      type:Number,
+      required:true
+    }
+  },
+  setup(props, context){
+    const selectCard = ()=>{
+      context.emit('select-card', {
+        position:props.position
+      })
+    }
+    return{
+      selectCard
     }
   }
+
 
 }
 </script>
 
 <style scoped>
+.card{
+  border: 5px solid #ccc;
+  position: relative;
+}
+.card-face.is-front{
+  background-color: red;
+  color: white;
+}
+.card-face.is-back{
+  background-color: blue;
+  color: white;
+}
+.card-face{
+  width:100%;
+  height: 100%;
+  position:absolute;
 
+}
 </style>
